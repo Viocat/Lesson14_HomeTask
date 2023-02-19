75275bd7f4b3ea47213ea1f6a5e8f1b927284644
Viocat <44540969+Viocat@users.noreply.github.com>
2023-02-20 01:23:30 +0200

    Add files via upload

---
+++ dz28/js/dz28.js
@@ -0,0 +1,32 @@
+"use strict";
+
+void
+
+function () {
+
+    // Вам нужно написать функцию, которая принимает один параметр. При первом вызове она его запоминает, при втором — суммирует переданный параметр с тем, что передали первый раз и тд. Например: sum(3) = 3; sum(5) = 8; sum(20) = 28
+
+    // function sumClosed(number) {
+    //     let result = 0;
+    //     sum(number);
+
+    //     function sum(n) {
+    //         return result += n;
+    //     }
+    //     return sum;
+    // }
+
+    const sumClosed = (number) => {
+        let result = 0;
+        sum(number);
+        const sum = n => result += n;
+        return sum;
+    }
+
+    const sum = sumClosed(0);
+
+    console.log(sum(4));
+    console.log(sum(6));
+    console.log(sum(9));
+
+}()
