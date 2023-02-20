4315f3e413943032669c5307c8ce850a4eb815e8
Viocat <44540969+Viocat@users.noreply.github.com>
2023-02-20 02:29:04 +0200

    Update dz29.js

--- dz29/js/dz29.js
+++ dz29/js/dz29.js
@@ -4,14 +4,14 @@ void function(){
 
     //Сделайте функцию, каждый вызов который будет генерировать случайные числа от 1 до 100, но так, чтобы они не повторялись, пока не будут перебраны все числа из этого промежутка. Решите задачу через замыкания — в замыкании должен хранится массив чисел, которые уже были сгенерированы функцией.
 
-    function randGen(){
-        const arr=[];
-        function mainGen(){
-            let rand=Math.round(Math.random()*99+1);
-            if(!arr.includes(rand)){
+  const randGen = () => {
+        const arr = [];
+        const mainGen = () => {
+            let rand = Math.round(Math.random() * 99 + 1);
+            if (!arr.includes(rand)) {
                 arr.push(rand);
                 return arr;
-            }else{
+            } else {
                 return mainGen();
             }
         }
